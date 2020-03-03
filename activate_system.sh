bd882163c9e815e660d239481918e16796a5bee9
fede <fede.herz@gmail.com>
2020-03-03 19:40:27 -0300

    connect to dropbox

--- activate_system.sh
+++ activate_system.sh
@@ -3,12 +3,12 @@
 # This script will enable IR movement sensor
 
 # Change to Desktop directory
-cd /home/pi/hometasks
+cd /home/pi/rpi-home1
 
 # log process start
 echo "`date` - start activate_system" > log_files/rpht.log
 
 # start flask app
-python3 app.py
+python3 app-silent.py
 
 # TODO enable IR sensor
--- deactivate_system.sh
+++ deactivate_system.sh
@@ -3,7 +3,7 @@
 # This script will disable IR movement sensor
 
 # Change to Desktop directory
-cd /home/pi/hometasks
+cd /home/pi/rpi-home1
 
 # log process start
 echo "`date` - start deactivate_system" > log_files/rpht.log
