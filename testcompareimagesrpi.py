b1f40f81765af9f2610089e3bc0710da5b5d6a91
fede <fede.herz@gmail.com>
2020-03-15 11:03:53 -0300

    fixes

--- testcompareimagesrpi.py
+++ testcompareimagesrpi.py
@@ -1,6 +1,5 @@
 # import the necessary packages
 from skimage import metrics
-import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
@@ -23,18 +22,8 @@ def compare_images(imageA, imageB, title):
     m = mse(imageA, imageB)
     s =  metrics.structural_similarity(imageA, imageB)
     # setup the figure
-    fig = plt.figure(title)
-    plt.suptitle("MSE: %.2f, SSIM: %.2f" % (m, s))
-    # show first image
-    ax = fig.add_subplot(1, 2, 1)
-    plt.imshow(imageA, cmap=plt.cm.gray)
-    plt.axis("off")
-    # show the second image
-    ax = fig.add_subplot(1, 2, 2)
-    plt.imshow(imageB, cmap=plt.cm.gray)
-    plt.axis("off")
-    # show the images
-    plt.show()
+    print(title)
+    print("MSE: %.2f, SSIM: %.2f" % (m, s))
 
 
 # load the images -- the original, the original + contrast,
@@ -47,18 +36,6 @@ original = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
 contrast = cv2.cvtColor(contrast, cv2.COLOR_BGR2GRAY)
 shopped = cv2.cvtColor(shopped, cv2.COLOR_BGR2GRAY)
 
-# initialize the figure
-fig = plt.figure("Images")
-images = ("Original", original), ("Contrast", contrast), ("Photoshopped", shopped)
-# loop over the images
-for (i, (name, image)) in enumerate(images):
-    # show the image
-    ax = fig.add_subplot(1, 3, i + 1)
-    ax.set_title(name)
-    plt.imshow(image, cmap = plt.cm.gray)
-    plt.axis("off")
-# show the figure
-plt.show()
 # compare the images
 compare_images(original, original, "Original vs. Original")
 compare_images(original, contrast, "Original vs. Contrast")
